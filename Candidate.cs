292aa08008c33c6b8c2157e1006c66596bf363d9
cat32tw <mike1993xd@gmail.com>
2022-02-14 20:41:51 +0800

    第四章習題3

---
+++ Candidate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candidate_Ex4_3
+{
+    internal class Candidate
+    {
+        public string Name { get; set; }
+        private static int num;
+        public static int Num
+        {
+            get { return num; }
+        }
+        public Candidate()
+        {
+            num++;
+        }
+        public Candidate(string name)
+        {
+            num++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Candidate_Ex4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Candidate per1 = new Candidate();
+            per1.Name = "林來來";
+            Console.WriteLine("候選人姓名:{0}", per1.Name);
+            Console.WriteLine("第 {0} 號登記", Candidate.Num);
+            Console.WriteLine("----------------------------");
+            Candidate per2 = new Candidate();
+            per2.Name = "蔡小小";
+            Console.WriteLine("候選人姓名:{0}", per2.Name);
+            Console.WriteLine("第 {0} 號登記", Candidate.Num);
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("目前登記總人數:{0}人", Candidate.Num);
+            Console.Read();
+        }
+    }
+}
